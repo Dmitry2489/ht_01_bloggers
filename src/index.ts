3268af02bcba0de5f8b1ac5afbfbc1cadbdf2711
Dmitry2489 <iva.klicunov2489@mail.ru>
2022-06-15 19:10:58 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser'
+const app = express()
+
+app.use(bodyParser.json())
+
+const port = 5000
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, Main page Bloggers!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
